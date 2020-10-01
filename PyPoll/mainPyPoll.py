4b5676e5072be6b38566bba626233435e078bd78
sambarton10 <69605114+sambarton10@users.noreply.github.com>
2020-09-30 21:06:44 -0500

    Added CVS Reader to PyBank Script

--- PyPoll/mainPyPoll.py
+++ PyPoll/mainPyPoll.py
@@ -1,1 +1,26 @@
-print ('Hello World')
+
+'Import
+
+import os
+import csv
+
+with open('/Users/sambarton/Python_Homework1/PyPoll/Resources/election_data.csv', 'r') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        print(row)
+
+
+
+
+'Total Number of Months
+
+
+
+'Total Number of "Profit/Losses" Over The Period
+
+'Average Of Changes "Profit/Losses" Over The Period
+
+'Greatest Increase In Profits
+
+'Greatest Decrease In Losses
+
