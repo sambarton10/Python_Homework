4f6968269c499e22c1d14a05794b52a5b3f49820
sambarton10 <69605114+sambarton10@users.noreply.github.com>
2020-10-03 18:37:15 -0500

    Playing aroun with pybank, cannot calculate average, greatest increase, lowest increase

--- PyBank/mainPyBank.py
+++ PyBank/mainPyBank.py
@@ -3,6 +3,9 @@
 import os
 import csv
 
+
+
+#Find the Total Profit/Loss
 with open('/Users/sambarton/Python_Homework1/PyBank/Resources/budget_data.csv', 'r') as csvfile:
     reader = csv.DictReader(csvfile)
     total = 0
@@ -10,25 +13,40 @@ with open('/Users/sambarton/Python_Homework1/PyBank/Resources/budget_data.csv', 
         total = sum(int(r[1]) for r in csv.reader(csvfile))
     print(total)
 
+#Counting the Total Number Of Months
+
 with open('/Users/sambarton/Python_Homework1/PyBank/Resources/budget_data.csv', 'r') as csvfile:
     reader = csv.DictReader(csvfile)
     TotalMonths=len(list(reader))
     print(TotalMonths)
     
+#Average change over entire period
+
+import csv
+import os
+
 with open('/Users/sambarton/Python_Homework1/PyBank/Resources/budget_data.csv', 'r') as csvfile:
-    reader = csv.DictReader(csvfile) 
-    next(reader)
-    for row in csvfile:
-        x = max(csvfile)
-        print(x)
-    
-with open('/Users/sambarton/Python_Homework1/PyBank/Resources/budget_data.csv', 'r') as csvfile:
-    reader = csv.DictReader(csvfile) 
-    next(reader)
-    for row in csvfile:
-        x = min(csvfile)
-        print(x)
+        reader = csv.DictReader(csvfile)
         
+        next(reader)
+        change2=0
+        
+        for row in reader:
+             
+             print(row['Profit/Losses'])
+             
+        
+        
+#Subtracting every current row from previous row, taking sum of all rows then finding average.
+
+        
+        
+#Finding greatest increase between months
+
+
+
+#Finding greatest decrease between months
+   
     
 print('Finacial Analysis')
 print('-----------------')
@@ -36,6 +54,25 @@ print('Total Months:', TotalMonths)
 print('Total Profit/Losses:', total)
 
 
+newdict = {'Date': 'Jun-2016', 'Profit/Losses': '712961'}
+print(newdict['Profit/Losses'])
 
+list2=[2,4,7,8]
+list2.append(10)
+for x in range(5):
+    print(list2[x])
+
+list3=[]
+for x in range(5):
+    list3.append(list2[x]-list2[x-1])
     
+print(list3[1:4])
 
+list5=[10,20,30,40]
+
+list6=[]
+
+for x in range(4):
+    list6.append(list5[x]-list5[x-1])
+print(list6)
+    
--- PyPoll/mainPyPoll.py
+++ PyPoll/mainPyPoll.py
@@ -10,3 +10,12 @@ with open('/Users/sambarton/Python_Homework1/PyPoll/Resources/election_data.csv'
     for row in reader:
         TotalVotes += 1   
     print(TotalVotes)
+    
+with open('/Users/sambarton/Python_Homework1/PyPoll/Resources/election_data.csv', 'r') as csvfile:
+    reader = csv.DictReader(csvfile)
+    CandidateOptions = []
+    next(reader)
+    for row in reader:
+        
+        
+    
